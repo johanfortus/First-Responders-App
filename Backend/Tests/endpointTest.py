89f35bf9ec6cd30988f469f7bb88b1983e112169
Anh Le <127444016+vule19@users.noreply.github.com>
2025-09-27 22:42:17 -0400

    Revert "db"

--- Backend/Tests/endpointTest.py
+++
@@ -1,7 +0,0 @@
-# curl -X POST http://127.0.0.1:5000/users/123/calls \
-# -H "Content-Type: application/json" \
-# -d '{
-#   "transcripts": "Caller reports fire at 5th street",
-#   "severityScore": 4,
-#   "date": "2025-09-27T18:30:00"
-# }'
