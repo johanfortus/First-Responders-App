bb6b9900e86b168726dc1b910de49b4e5fb4e6a4
Destin Gollamudi <destingollamudi@gmail.com>
2025-09-28 07:20:41 -0400

    Resolved conflicts

--- Frontend/app/(tabs)/_layout.tsx
+++ Frontend/app/(tabs)/_layout.tsx
@@ -1,26 +1,32 @@
-import { Tabs } from 'expo-router'
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { HapticTab } from '@/components/haptic-tab';
+import { IconSymbol } from '@/components/ui/icon-symbol';
+import { Colors } from '@/constants/theme';
+import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export default function _layout() {
   return (
     <Tabs screenOptions={{
         tabBarShowLabel: false,
         headerShown: false,
-        tabBarStyle: styles.tabBar,
-    }}>
-        <Tabs.Screen name='index'/>
-
+        tabBarButton: HapticTab,
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
     </Tabs>
   )
 }
-
-const styles = StyleSheet.create({
-    tabBar: {
-        borderTopWidth: 0,
-        backgroundColor: 'gray',
-        height: 40,
-        position: 'absolute',
-        paddingBottom: 8,
-    }
-})
