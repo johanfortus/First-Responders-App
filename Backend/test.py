c8f5603dad5cf63c800b742ab31c5f182d4a4794
vule19 <anhvle1901@gmail.com>
2025-09-27 23:04:26 -0400

    upload testing to test endpoint

---
+++ Backend/test.py
@@ -0,0 +1,26 @@
+# import requests
+
+# url = "http://127.0.0.1:5000/call"
+# data = {
+#     "callID": 5,
+#     "userID": "u001",
+#     "transcripts": "This is a sample transcript.",
+#     "severityScore": 5,
+#     "date": "2025-09-27T22:00:00"
+# }
+
+# response = requests.post(url, json=data)
+# print(response.status_code)
+# print(response.json())
+
+
+import requests
+
+url = "http://127.0.0.1:5000/user/u001"
+data = {
+    "callID": 6
+}
+
+response = requests.put(url, json=data)
+print(response.status_code)
+print(response.json())
